356e5de1d647c9814be6e921da77cbc722119c3e
Exesnik <92322311+Exesnik@users.noreply.github.com>
2022-02-25 14:16:36 +0400

    .

---
+++ Forest Survival/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D), typeof(BoxCollider2D))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rigidbody;
+    [SerializeField]
+    private FixedJoystick _joystick;
+
+    [SerializeField]
+    private float _moveSpeed;
+
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed);
+    }
+}
---
+++ Forest Survival/Assets/Scripts/Shooting.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
+
+
+    void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * bulletSpeed;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+}
