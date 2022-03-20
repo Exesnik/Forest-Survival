1208a777bf96656d90cbb8456094712996175e4f
Exesnik <92322311+Exesnik@users.noreply.github.com>
2022-03-20 10:36:14 +0400

    .

--- Forest Survival/Assets/Scripts/ShootingCore.cs
+++ Forest Survival/Assets/Scripts/ShootingCore.cs
@@ -6,11 +6,28 @@ public class ShootingCore : MonoBehaviour
 {
     [SerializeField]
     private FixedJoystick _joystick;
+    [SerializeField]
+    private Rigidbody2D _rbShootingCore;
+    private Vector2 _rotation;
+
+    public float speed = 10; // скорость пули
+    public Rigidbody2D bullet; // префаб нашей пули
+    public Transform gunPoint; // точка рождения
+    public float fireRate = 1; // скорострельность
+    private float curTimeout;
+
+    private void Start()
+    {
+        
+    }
 
 
     private void FixedUpdate()
     {
         LookRotation();
+        Fire();
+
+
     }
 
     private void LookRotation()
@@ -18,8 +35,24 @@ public class ShootingCore : MonoBehaviour
 
         if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
         {
-            float AngleA = Mathf.Atan2(-1 * _joystick.Horizontal, _joystick.Vertical) * Mathf.Rad2Deg;
+            float AngleA = Mathf.Atan2(_joystick.Horizontal, _joystick.Vertical) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, AngleA);
+
+
+        }
+
+
+
+    }
+    void Fire()
+    {
+        curTimeout += Time.deltaTime;
+        if (curTimeout > fireRate)
+        {
+            curTimeout = 0;
+            Rigidbody2D clone = Instantiate(bullet, gunPoint.position, Quaternion.identity) as Rigidbody2D;
+            clone.velocity = transform.TransformDirection(gunPoint.up * speed);
+            clone.transform.up = gunPoint.up;
         }
     }
 }
